8c8eaf4a879ed06badc5de92c22c15b2f1c6e5ad
Sven Arnold <sa@triarc-labs.com>
2015-12-09 10:49:05 +0100

    Bumped version to 1.0.4

--- tlFileService.d.ts
+++ tlFileService.d.ts
@@ -8,7 +8,7 @@ declare module Triarc.Web {
         static serviceId: string;
         static $inject: string[];
         constructor($q: angular.IQService);
-        downloadFile(url: string, fileName: string, isMobile: boolean, downloadEvents?: IDownloadEvents): ng.IPromise<boolean>;
+        downloadFile(url: string, fileName: string, isMobile: boolean, downloadEvents?: IDownloadEvents): angular.IPromise<boolean>;
         private mobileDownload(url, name);
     }
 }
